2f6b575d268136dddd54cc69c768251c40be76c3
Jacob Adamson <jakeladamson@gmail.com>
2021-09-06 19:24:51 -0600

    adding init files

---
+++ server.js
@@ -0,0 +1,44 @@
+//require modules
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+require("dotenv").config();
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up sessions with cookies
+const sess = {
+  secret: "Super secret secret",
+  cookie: {
+    // Stored in milliseconds (86400 === 1 day)
+    maxAge: 86400,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create();
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening on PORT " + PORT));
+});
