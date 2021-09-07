ac822ae11dbb2367acd2e6eaaeab96c48698a24c
Jacob Adamson <jakeladamson@gmail.com>
2021-09-07 15:54:30 -0600

    updated routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,64 +6,42 @@ const router = require("express").Router();
 //establish routes
 router.get("/", (req, res) => {
   Post.findAll({
-    attributes: ["id", "title", "content", "created_at"],
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
+    include: [User],
   })
     .then((dbData) => {
       const userPosts = dbData.map((post) => post.get({ plain: true }));
-      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+      res.render("homepage", { userPosts });
     })
     .catch((err) => {
       res.status(500).json(err);
     });
 });
 //signup route
-router.get("/signup", (req, res) => {
-  res.render("signup");
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
 //route by id
 router.get("/post/:id", (req, res) => {
-  Post.findOne({
+  Post.findByPk({
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "content", "title", "created_at"],
     include: [
+      User,
       {
         model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
+        include: [User],
       },
     ],
   })
     .then((dbData) => {
       if (!dbData) {
-        res.status(404).json({ message: "Couldn't find post this id" });
+        res.status(404).json({ message: "Couldn't find post with this id" });
         return;
       }
       const post = dbData.get({ plain: true });
-      res.render("single-post", { post, loggedIn: req.session.loggedIn });
+      res.render("single-post", { post });
     })
     .catch((err) => {
       res.status(500).json(err);
--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,7 @@
 // auth function, if user is not logged in redirect
 //if logged in, then next route
 const withAuth = (req, res, next) => {
-  if (!req.sesssion.user_id) {
+  if (!req.session.user_id) {
     res.redirect("/login");
   } else {
     next();
